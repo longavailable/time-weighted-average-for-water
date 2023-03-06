43465d336b98760355b84064e0bb7c4fef81cae3
Xiaolong "Bruce" Liu <liuxiaolong125@gmail.com>
2023-03-06 21:49:20 +0800

    Update twaw.py

--- twaw/twaw.py
+++ twaw/twaw.py
@@ -1,14 +1,69 @@
 # -*- coding: utf-8 -*-
 '''
-* Updated on 2023/03/02
+* Updated on 2023/03/06
 * python3
 **
 * Calculate time(hour)-weighted daily average
 '''
 
 from datetime import datetime, timedelta
+from multiprocessing.dummy import Pool as ThreadPool
+from itertools import repeat
 import pandas as pd
 import numpy as np
+
+def addDays(startDate, n, skip_leap_days=False):
+	'''A function to operate addition for a date.
+	
+	Parameters:
+		startDate:
+			Type: datetime.date
+		n:
+			Type: int
+		skip_leap_days:
+			Type: boolean
+			Default: False
+	Returns:
+		A datetime.date object
+	'''
+	d = startDate + timedelta(n)
+	if skip_leap_days and d.month==2 and d.day==29: return None
+	return d
+
+def daterange(startDate, endDate, skip_leap_days=False):
+	'''A function to abstract the iteration over the range of dates.
+	
+	Parameters:
+		startDate:
+			Type: datetime.date
+		endDate:
+			Type: datetime.date
+		skip_leap_days:
+			Type: boolean
+			Default: False
+	Returns:
+		A list
+	'''
+	
+	'''
+	for n in range(int((endDate - startDate).days)):
+		yield startDate + timedelta(n)
+	'''
+	
+	'''
+	# single thread version
+	dates = []
+	for n in range(int((endDate - startDate).days)):
+		d = addDays(startDate, n, skip_leap_days)
+		dates = dates + [d]
+	return dates
+	'''
+	# parallel
+	pool = ThreadPool(4)
+	dates = pool.starmap(addDays, zip(repeat(startDate), list(range(int((endDate - startDate).days))), repeat(skip_leap_days)))
+	pool.close()
+	pool.join()
+	return list(filter(lambda d: d is not None, dates))
 
 def filterByTime(pdf, d0, d1, timeHeader):
 	'''Filter subset by datetime specified its header
@@ -119,8 +174,64 @@ def weightedAverage(data, weights = None):
 		float 
 	'''
 	return np.average(data, weights=weights)
+
+def dailyAverageCore(pdf, itemHeader, timeHeader, date):
+	'''Calculate daily average of variable (water level/flow) weighted for a specific date
 	
-def dailyAverage(pdf, itemHeader, timeHeader):
+	Parameters:
+		pdf: source data to use
+			Type: pandas.DataFrame object
+		itemHeader: label(s)/header(s) of variable column
+			Type: string or list of string
+		timeHeader: label/header of time column
+			Type: string
+		date: specific date
+			Type: datetime.date object
+		
+	return:
+		a record of dictionary 
+	'''
+
+	d1 = datetime.combine(date, datetime.min.time())	# 0:00 of the current day (d1)
+	d0 = d1 -  timedelta(days=1)	# 0:00 of the previous day (d0)
+	d2 = d1 +  timedelta(days=1)	# 0:00 of the next day (d2)
+	d3 = d1 +  timedelta(days=2)	# 0:00 of the next two day (d3)
+	
+	record = { 'year': d1.year, 'month': d1.month, 'day': d1.day }
+	
+	for item in itemHeader:
+		# initialize
+		record[item] = np.nan
+		
+		subdata = filterByTime(pdf, d1, d2, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
+		
+		# case 0: do nothing
+		if len(subdata) == 0: continue
+		
+		# case 1: use it as the daily average if only one gauged data
+		if len(subdata) == 1: 
+			record[item] = round(subdata[item][0],2)
+		else:
+			# first data
+			if subdata.iloc[0][timeHeader].to_pydatetime() != d1:
+				subdata1 = filterByTime(pdf, d0, d1, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
+				subdata = addFirst(subdata1, subdata, item, timeHeader)
+			
+			# last data
+			if subdata.iloc[-1][timeHeader].to_pydatetime() != d2:
+				subdata2 = filterByTime(pdf, d2, d3, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
+				subdata = addLast(subdata, subdata2, item, timeHeader)
+			
+			# weights
+			weights = weightsCalculator(subdata[timeHeader].to_numpy())
+			
+			# daily average
+			va = weightedAverage(subdata[item].to_numpy(), weights=weights)
+			record[item] = round(va,2)
+	
+	return record
+	
+def dailyAverage(pdf, itemHeader, timeHeader, parallel=False, poolSize=4):
 	'''Calculate daily average of variable (water level/flow) weighted along time
 	
 	Parameters:
@@ -130,54 +241,30 @@ def dailyAverage(pdf, itemHeader, timeHeader):
 			Type: string or list of string
 		timeHeader: label/header of time column
 			Type: string
+		parallel: 
+			Type: boolean
+		poolSize: processes
+			Type: int
 		
 	return:
-		list of dictionaries 
+		list of dictionaries
 	'''
 	# start date and enddate
-	time0 = datetime.combine(pdf[timeHeader].min().date(), datetime.min.time())
-	time1 = datetime.combine(pdf[timeHeader].max().date(), datetime.min.time())
+	date0 = pdf[timeHeader].min().date()
+	date1 = pdf[timeHeader].max().date()
 	
-	records = []
-	while time0 < time1:
-		# 0:00 of the previous day (d0), current day (d1), next (d2), next two (d3)
-		d1, d2 = time0, time0 +  timedelta(days=1)
-		d0, d3 = time0 -  timedelta(days=1), time0 + timedelta(days=2)
-		
-		record = { 'year': d1.year, 'month': d1.month, 'day': d1.day }
-		
-		for item in itemHeader:
-			# initialize
-			record[item] = np.nan
-			
-			subdata = filterByTime(pdf, d1, d2, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
-			
-			# case 0: do nothing
-			if len(subdata) == 0: continue
-			
-			# case 1: use it as the daily average if only one gauged data
-			if len(subdata) == 1: 
-				record[item] = round(subdata[item][0],2)
-			else:
-				# first data
-				if subdata.iloc[0][timeHeader].to_pydatetime() != d1:
-					subdata1 = filterByTime(pdf, d0, d1, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
-					subdata = addFirst(subdata1, subdata, item, timeHeader)
-				
-				# last data
-				if subdata.iloc[-1][timeHeader].to_pydatetime() != d2:
-					subdata2 = filterByTime(pdf, d2, d3, timeHeader).dropna(subset=item).sort_values(by=timeHeader).reset_index(drop=True)
-					subdata = addLast(subdata, subdata2, item, timeHeader)
-				
-				# weights
-				weights = weightsCalculator(subdata[timeHeader].to_numpy())
-				
-				# daily average
-				va = weightedAverage(subdata[item].to_numpy(), weights=weights)
-				record[item] = round(va,2)
-		
-		records.append(record)
-		
-		# next day
-		time0 = d2
+	# parallel
+	if parallel:
+		pool = ThreadPool(poolSize)
+		records = pool.starmap(dailyAverageCore, zip(repeat(pdf), repeat(itemHeader), repeat(timeHeader), daterange(date0, date1)))
+		pool.close()
+		pool.join()
+	else:
+		records = []
+		while date0 < date1:
+			record = dailyAverageCore(pdf, itemHeader, timeHeader, date0)
+			records.append(record)
+			# next day
+			date0 = date0 +  timedelta(1)
+	
 	return records
